776ab4a41ffb9f885da4c9e78438a599db888091
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2023-12-14 13:36:53 +0530

    add API for user

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->group(function() {
 
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/users', UserController::class);
 
     Route::get('/user', function (Request $request) {
         return $request->user();
